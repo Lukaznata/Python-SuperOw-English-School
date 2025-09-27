7e26fe7730108e5f988252c3c41fd495734e1d3e
Lucas Nathan <lucas18tonin@gmail.com>
2025-09-27 14:51:13 -0300

    Alterei as variaveis de ambiente para facilitar a leitura

--- app/core/database.py
+++ app/core/database.py
@@ -6,9 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# VARIÁVEIS DE AMBIENTE PARA CONEXÃO COM O BANCO DE DADOS
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Dependency
